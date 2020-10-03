6b13752a6117b4dfce802e6b496fa3cf2e157502
Kevin Avery <Averykev@gmail.com>
2020-10-03 15:37:37 -0700

    rough draft code for earthquakes and maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,63 @@
-//creating map object
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
-  
-//add a tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-}).addTo(myMap);
 
 //store API query url
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson"
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-//grab the data with D3
-d3.json(url, function(response){
-
-    L.geoJson(response).addTo(myMap);
+//perform a GET request to the query URL
+d3.json(url, function(data){
+    createFeatures(data.features);
 });
+
+function createFeatures(earthquakeData){
+    function onEachFeature(feature, layer){
+        layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    var earthquakes = L.geoJson(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+    
+    createMap(earthquakes);
+}
+
+
+
+function createMap(earthquakes) {
+
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
+    });
+
+    var satMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/satellite-streets-v11",
+        accessToken: API_KEY
+    });
+
+
+    var baseMaps = {
+        "Light Map": lightMap,
+        "Satellite Map": satMap
+    };
+
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    var myMap = L.map("map", {
+        center: [37.09,-95.71],
+        zoom: 5,
+        layers: [lightMap, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
