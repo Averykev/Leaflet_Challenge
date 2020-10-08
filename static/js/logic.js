dbba46c2a25f98dc025b1fcc6b07366488eb4cd0
Kevin Avery <Averykev@gmail.com>
2020-10-07 19:35:08 -0700

    updated legend & css style sheet

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,38 +1,7 @@
 
-//store API query url
+//store API query url's
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-
-//create a function for marker size
-
-function markerSize(magnitude){
-    return magnitude *4.25;
-}
-
-//create a function for marker color
-
-function markerColor(depth){
-    if (depth <= 5) {
-        return "#32dcff"
-    }
-    else if (depth <=10) {
-        return "#ff99ff"
-    }
-    else if (depth <=20) {
-        return "#32ff32"
-    }
-    else if (depth <=30) {
-        return "#e5ff32"
-    }
-    else if (depth <=40) {
-        return "#ffb232"
-    }
-    else {
-        return "#ff3232"
-    }
-}
-
-
+var tectonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
 //perform a GET request to the query URL
 d3.json(url, function(data){
@@ -49,10 +18,10 @@ function createFeatures(earthquakeData){
     var earthquakes = L.geoJson(earthquakeData, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
-                radius: markerSize(feature.properties.mag),
+                radius: markerRadius(feature.properties.mag),
                 fillColor: markerColor(feature.geometry.coordinates[2]),
                 color: "black",
-                fillOpacity: 1,
+                fillOpacity: .8,
                 stroke: true,
                 weight: .5
             })
@@ -83,9 +52,19 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
+    var terrainMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/outdoors-v11",
+        accessToken: API_KEY
+    });
+
     var baseMaps = {
         "Light Map": lightMap,
-        "Satellite Map": satMap
+        "Terrain Map": terrainMap,
+        "Satellite Map": satMap        
     };
 
     var overlayMaps = {
@@ -115,7 +94,6 @@ function createMap(earthquakes) {
             labels = ["< 5m", "5m - 10m", "10m - 20m", "20m - 30m", "30m - 40m", "> 40m"];
 
         for (var i = 0; i <colors.length; i++) {
-
             div.innerHTML += '<li style="padding: 4px; background-color:' + colors[i] + '">' + labels[i] + '</li>';
             }
 
@@ -126,4 +104,29 @@ function createMap(earthquakes) {
 };
 
 
+//create a function for marker size
+function markerRadius(magnitude){
+    return magnitude *4.25;
+};
 
+//create a function for marker color
+function markerColor(depth){
+    if (depth <= 5) {
+        return "#32dcff"
+    }
+    else if (depth <=10) {
+        return "#ff99ff"
+    }
+    else if (depth <=20) {
+        return "#32ff32"
+    }
+    else if (depth <=30) {
+        return "#e5ff32"
+    }
+    else if (depth <=40) {
+        return "#ffb232"
+    }
+    else {
+        return "#ff3232"
+    }
+};
